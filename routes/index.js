6ade931b7b0226a6b6eb12c05685208e29fef763
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-01 17:00:54 -0500

    Update routes index and polls

--- routes/index.js
+++ routes/index.js
@@ -9,52 +9,52 @@ router.get("/", (req, res) => {
   res.render("index", { user: req.session.user });
 });
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
-
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user  = await startSession(email, password);
-    const polls = await getPolls();
-    req.session.user = user;
-    res.render('polls', { user: req.session.user, polls, message: `Welcome ${user.name}` });
-  } catch (error) {
-    res.render('login', { error: error.message });
-  }
-});
-
-router.get("/signup", (req, res) => {
-  res.render("signup");
-});
-
-router.post("/signup", async (req, res) => {
-  const { name, email, password } = req.body;
-  try {
-    const data = await createUser({ name, email, password });
-    res.render("login", { success: data });
-  } catch (error) {
-    if (error.name === "ValidationError") {
-      const { name, email } = error.errors;
-      let errorsHandler;
-      if (name && email) {
-        errorsHandler = { name: name.message, email: email.message };
-      } else if (name) {
-        errorsHandler = { name: name.message };
-      } else {
-        errorsHandler = { email: email.message };
-      }
-      res.render("signup", errorsHandler);
-    } else {
-      res.render("signup", { error: error.message });
-    }
-  }
-});
-
 router.get("/logout", (req, res) => {
   req.session = null;
   res.render("login");
 });
+
+router.route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post( async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user  = await startSession(email, password);
+      const polls = await getPolls();
+      req.session.user = user;
+      res.render('polls', { user: req.session.user, polls, message: `Welcome ${user.name}` });
+    } catch (error) {
+      res.render('login', { error: error.message });
+    }
+  })
+
+router.route("/signup")
+  .get((req, res) => {
+    res.render("signup");
+  })
+  .post( async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+      const data = await createUser({ name, email, password });
+      res.render("login", { success: data });
+    } catch (error) {
+      if (error.name === "ValidationError") {
+        const { name, email } = error.errors;
+        let errorsHandler;
+        if (name && email) {
+          errorsHandler = { name: name.message, email: email.message };
+        } else if (name) {
+          errorsHandler = { name: name.message };
+        } else {
+          errorsHandler = { email: email.message };
+        }
+        res.render("signup", errorsHandler);
+      } else {
+        res.render("signup", { error: error.message });
+      }
+    }
+  });
 
 module.exports = router;
--- routes/poll.js
+++ routes/poll.js
@@ -12,53 +12,63 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/create", requireUser, (req, res) => {
-  res.render("poll-form");
-});
-
-router.post("/create", async (req, res) => {
-  try {
-    const { name, description, options } = req.body;
-    const poll = await createPoll( name, description, options, req.session.user._id);
-    req.session.message = 'Poll created successfully';
-    res.redirect(`/polls/${poll._id}/results`);
-  } catch (error) {
-    let handleErrors = {};
-    if (error.name === "ValidationError") {
-      const { name, description } = error.errors;
-      const arrayErrors = Object.keys(error.errors);
-      arrayErrors.forEach( arrayError => {
-        if (arrayError === 'name') {
-          handleErrors['name'] = name.message;
-        } else if (arrayError === 'description') {
-          handleErrors['description'] = description.message;
-        } else if (arrayError !== 'name' && arrayError !== 'description') {
-          handleErrors['optionsError'] = error.errors[`${arrayError}`].message;
-        }
-      });
-      if (!handleErrors.hasOwnProperty('user')) handleErrors['user'] = req.session.user;
-      res.render('poll-form', handleErrors);
-    } else {
-      res.render('poll-form', { user: req.session.user, error: error.message });
+router.route("/create")
+  .get( requireUser, (req, res) => {
+    res.render("poll-form");
+  })
+  .post( async (req, res) => {
+    try {
+      const { name, description, options } = req.body;
+      const poll = await createPoll( name, description, options, req.session.user._id);
+      req.session.message = 'Poll created successfully';
+      res.redirect(`/polls/${poll._id}/results`);
+    } catch (error) {
+      let handleErrors = {};
+      if (error.name === "ValidationError") {
+        const { name, description } = error.errors;
+        const arrayErrors = Object.keys(error.errors);
+        arrayErrors.forEach( arrayError => {
+          if (arrayError === 'name') {
+            handleErrors['name'] = name.message;
+          } else if (arrayError === 'description') {
+            handleErrors['description'] = description.message;
+          } else if (arrayError !== 'name' && arrayError !== 'description') {
+            handleErrors['optionsError'] = error.errors[`${arrayError}`].message;
+          }
+        });
+        if (!handleErrors.hasOwnProperty('user')) handleErrors['user'] = req.session.user;
+        res.render('poll-form', handleErrors);
+      } else {
+        res.render('poll-form', { user: req.session.user, error: error.message });
+      }
     }
-  }
-});
+  });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const poll = await getPoll(req.params.id);
-    res.render('poll-form-vote', { poll, user: req.session.user });
-  } catch {
-    res.render('polls', { error: error.message });
-  }
-});
-
-router.post("/:id", async (req, res) => {
+router.route("/:id")
+  .get( async (req, res) => {
+    try {
+      const poll = await getPoll(req.params.id);
+      res.render('poll-form-vote', { poll, user: req.session.user });
+    } catch {
+      res.render('polls', { error: error.message });
+    }
+  })
+  .post( async (req, res) => {
   try {
     await addVote(req.params.id, req.body.option);
     res.redirect(`/polls/${req.params.id}/results`);
   } catch (error) {
     res.redirect(`/${req.params.id}`);
+  }
+});
+
+router.post("/delete/:id", async (req, res) => {
+  try {
+    const message = await deletePoll(req.params.id);
+    const polls = await getPolls();
+    res.render('polls', { polls, message, user: req.session.user });
+  } catch {
+    res.render('polls', { error: error.message });
   }
 });
 
@@ -68,16 +78,6 @@ router.get("/:id/results", async (req, res) => {
     const message = req.session.message;
     req.session.message = null;
     res.render('results', { poll, user: req.session.user, message });
-  } catch {
-    res.render('polls', { error: error.message });
-  }
-})
-
-router.post("/delete/:id", async (req, res) => {
-  try {
-    const message = await deletePoll(req.params.id);
-    const polls = await getPolls();
-    res.render('polls', { polls, message, user: req.session.user });
   } catch {
     res.render('polls', { error: error.message });
   }
