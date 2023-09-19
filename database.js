3dfa0ae70f5aec45307f0522a1b878ac218d6227
Diego Cardenas <diego.cardenasaleg@gmail.com>
2023-09-19 10:19:16 -0500

    updated logic to handle moongose conection and engine to run node on deploy

--- database.js
+++ database.js
@@ -1,10 +1,17 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
 
+const MONGOOSE_URL = process.env['MONGOLAB_URI'] || "mongodb://127.0.0.1:27017/polls"
 // Set database connection
 mongoose
-  .connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/polls", {
+  .connect(MONGOOSE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-  })
-  .catch((error) => console.log(error));
+  });
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.info("DB is connected successfully");
+});
